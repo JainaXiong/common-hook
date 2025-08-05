a027e0fda1c2ac0eb6970793f5f88e579a2cac03
Jaina Xiong <17761608@qq.com>
2025-08-05 20:02:03 +0800

    ✨ feat: 更新版本和依赖，替换深度比较库
    - 将版本号更新至1.3.1
    - 将依赖项dequal替换为common-screw
    - 更新useDeepCompareMemoize钩子以使用新库
    - 更新类型声明文件以包含common-screw模块

--- src/_utils/useDeepCompareMemoize.ts
+++ src/_utils/useDeepCompareMemoize.ts
@@ -3,16 +3,16 @@
  * @Email: 17761608@qq.com
  * @Date: 2025-08-05 19:29:25
  * @LastEditors: Jaina Xiong
- * @LastEditTime: 2025-08-05 19:29:33
+ * @LastEditTime: 2025-08-05 19:52:16
  */
 import { useRef, useMemo } from 'react'
-import { dequal } from 'dequal'
+import { isEqual } from 'common-screw'
 
 export function useDeepCompareMemoize(dependencies: any) {
   const dependenciesRef = useRef(dependencies)
   const signalRef = useRef(0)
 
-  if (!dequal(dependencies, dependenciesRef.current)) {
+  if (!isEqual(dependencies, dependenciesRef.current)) {
     dependenciesRef.current = dependencies
     signalRef.current += 1
   }
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -3,9 +3,8 @@
  * @Email: 17761608@qq.com
  * @Date: 2025-07-09 12:43:07
  * @LastEditors: Jaina Xiong
- * @LastEditTime: 2025-08-05 19:30:56
+ * @LastEditTime: 2025-08-05 19:52:27
  */
 declare module 'react'
-declare module 'dequal'
 declare module 'common-hook'
 declare module 'common-screw'
